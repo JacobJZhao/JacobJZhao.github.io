a7599285fd3399e4f0d36c70634278a957b8a7f1
Jacob Zhao <32437488+JacobJZhao@users.noreply.github.com>
2019-10-29 20:09:41 -0700

    Discovered errors with piano

--- Processing/piano/sketch.js
+++ Processing/piano/sketch.js
@@ -1,12 +1,15 @@
 function preload() {
+  soundFormats('mp3');
   C = loadSound("./sounds/C.mp3");
  }
  
- function setup() {
-   createCanvas(200, 200);
-   background(0);
-   C.play();
- }
+function setup() {
+  createCanvas(200, 200);
+  background(0);
+  C.play();
+  C.setVolume(1);
+}
 
- function draw(){
- }
+function draw(){
+  //C.play();
+}
