4b2573bf4977f8a8f9c2123080a1c1da86651efd
Jacob Zhao <32437488+JacobJZhao@users.noreply.github.com>
2019-10-31 16:42:27 -0700

    first octave

--- Processing/piano/sketch.js
+++ Processing/piano/sketch.js
@@ -1,10 +1,13 @@
 function preload() {
   soundFormats('mp3', 'wav');
-  C4 = loadSound("./sounds/C4.mp3");
+  C4 = loadSound("./sounds/C4.wav");
   D4 = loadSound("./sounds/D4.wav");
   E4 = loadSound("./sounds/E4.wav");
   F4 = loadSound("./sounds/F4.wav");
   G4 = loadSound("./sounds/G4.wav");
+  A4 = loadSound("./sounds/A4.wav");
+  B4 = loadSound("./sounds/B4.wav");
+  C5 = loadSound("./sounds/C5.wav");
  }
  
 function setup() {
@@ -30,6 +33,15 @@ function draw(){
     }
     if(key === 'p'){
       G4.play();
+    }
+    if(key === '['){
+      A4.play();
+    }
+    if(key === ']'){
+      B4.play();
+    }
+    if(key === "\\"){
+      C5.play();
     }
   }
 }
